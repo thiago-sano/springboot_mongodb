e391d5cf9b67490b080370180e7fc7e8ab60557f
thiago.sano <thiago.sano@outlook.com>
2025-03-26 12:09:41 -0300

    feat: Conectando ao MongoDB com repository e service

---
+++ src/main/java/br/com/educandoweb/springbootmongodb/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.educandoweb.springbootmongodb.repository;
+
+import br.com.educandoweb.springbootmongodb.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository <User, String> {
+
+}
---
+++ src/main/java/br/com/educandoweb/springbootmongodb/services/UserService.java
@@ -0,0 +1,19 @@
+package br.com.educandoweb.springbootmongodb.services;
+
+import br.com.educandoweb.springbootmongodb.domain.User;
+import br.com.educandoweb.springbootmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
