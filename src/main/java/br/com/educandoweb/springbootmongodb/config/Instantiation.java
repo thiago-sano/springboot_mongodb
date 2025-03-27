9164bf1695146f9b67dab4a8c3543874fab9eef4
thiago.sano <thiago.sano@outlook.com>
2025-03-27 17:33:59 -0300

    feat: Acrescentando comentários aos posts

--- src/main/java/br/com/educandoweb/springbootmongodb/config/Instantiation.java
+++ src/main/java/br/com/educandoweb/springbootmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package br.com.educandoweb.springbootmongodb.config;
 import br.com.educandoweb.springbootmongodb.domain.Post;
 import br.com.educandoweb.springbootmongodb.domain.User;
 import br.com.educandoweb.springbootmongodb.dto.AuthorDTO;
+import br.com.educandoweb.springbootmongodb.dto.CommentDTO;
 import br.com.educandoweb.springbootmongodb.repository.PostRepository;
 import br.com.educandoweb.springbootmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,9 +39,17 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-        postRepository.saveAll(Arrays.asList(post1, post2));
 
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
         maria.getPosts().addAll(Arrays.asList(post1, post2));
         userRepository.save(maria);
+
     }
 }
