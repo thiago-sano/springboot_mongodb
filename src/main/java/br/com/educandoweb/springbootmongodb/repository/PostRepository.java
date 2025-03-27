3bc1e0f03f1ea7547a666a61ce39769e1fd53f4d
thiago.sano <thiago.sano@outlook.com>
2025-03-27 17:58:24 -0300

    feat: Consulta simples com query methods

--- src/main/java/br/com/educandoweb/springbootmongodb/repository/PostRepository.java
+++ src/main/java/br/com/educandoweb/springbootmongodb/repository/PostRepository.java
@@ -3,5 +3,9 @@ package br.com.educandoweb.springbootmongodb.repository;
 import br.com.educandoweb.springbootmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
